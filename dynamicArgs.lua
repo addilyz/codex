64eb137fbf4a6bcffe94d251ae72b90dea53fae0
addilyn <addilyz@proton.me>
2025-06-26 05:56:21 -0700

    private repo addition
    we are in need of a refactor for sure so just biting the bullet

---
+++ dynamicArgs.lua
@@ -0,0 +1,21 @@
+local dna = {}
+local props = {}
+local cases = {}
+function dna.add(arg,case)
+	local ti = table.insert
+	ti(props,arg)
+	ti(cases,case)
+end
+function dna.scan(ms,arg,b)
+	if ms~=arg then
+		cases[b](ms)
+	end
+end
+function dna.sweep(args)
+	local ss = string.sub
+	for a=1, #args, 1 do
+		for b=1, #props, 1 do
+		dna.scan(ss(args[a],props[b],""),args[a],b)
+	end
+end
+return dna
