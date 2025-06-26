c86991ad75eb3b87afb38f7b6371215ab0ecf268
addilyn <addilyz@proton.me>
2025-06-26 06:06:03 -0700

    performance locals

--- codex.lua
+++ codex.lua
@@ -70,13 +70,16 @@ function codex.add(key,tab)
 	end
 end
 
+local lg = love.graphics
+local le = love.event
+local lt = love.timer
 function love.run()
 	codex.handle("load",love.arg.parseGameArguments(arg), arg)
 	local dt = 0
 	return function()
-		if love.event then
-			love.event.pump()
-			for name, a,b,c,d,e,f in love.event.poll() do
+		if le then
+			le.pump()
+			for name, a,b,c,d,e,f in le.poll() do
 				if name == "quit" then
 					if not love.quit or not love.quit() then
 						return a or 0
@@ -85,14 +88,14 @@ function love.run()
 				codex.handle(name,a,b,c,d,e,f)
 			end
 		end
-		if love.timer then dt = love.timer.step() end
+		if lt then dt = lt.step() end
 		codex.handle("update",dt)
-		if love.graphics and love.graphics.isActive() then
-			love.graphics.origin()
-			love.graphics.clear(love.graphics.getBackgroundColor())
+		if lg and lg.isActive() then
+			lg.origin()
+			lg.clear(lg.getBackgroundColor())
 			codex.pages.draw()
-			love.graphics.present()
+			lg.present()
 		end
-		if love.timer then love.timer.sleep(0.001) end
+		if lt then lt.sleep(0.001) end
 	end
 end
