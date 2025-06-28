cb6b2ede5b42f5dd3ed3520a786a46619a887540
addilyn <addilyz@squick.dev>
2025-06-28 00:13:50 -0700

    updates

--- sugar.lua
+++ sugar.lua
@@ -6,6 +6,9 @@ local tf = false
 local uptime = 0
 local bootloader = "bits"
 local blargs = {}
+local lg = love.graphics
+local lw = love.window
+local mobile = false
 
 function sugar.load(args)
 	tf = dna.sweep(args)
@@ -19,15 +22,21 @@ end
 function sugar.update(dt)
 	uptime=uptime+dt
 	if uptime > .1 then
+		if lg.getWidth()>9 then mobile = true else 
+			lw.setMode(800,600)
+		end 
+	end
+	if uptime > .2 then
 		if tf == false then
 			local bl = require(bootloader)
 			codex.add("bootloader",bl)
 			if type(bl.load) ~= "nil" then
-				bl.load()
+				bl.load(blargs)
 			end
 		end
-		codex.remove("sugar")
+		codex.delete("sugar")
 	end
 end
 
 codex.add("sugar",sugar)
+return sugar
